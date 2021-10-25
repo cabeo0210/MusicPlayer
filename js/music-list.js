d7f7d2459d52d4cfec7d53a90aff5a473a4ee2a2
cabeo0210 <cabeo0210@gmail.com>
2021-10-25 14:18:36 +0700

    Upload

---
+++ js/music-list.js
@@ -0,0 +1,21 @@
+let allMusic = [
+  {
+    name: "Trời Giấu Trời Mang Đi",
+    artist: "Amee x ViruSs",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Giấc Mơ Rất Thơ",
+    artist: "Mer",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Tình Thật Tình Mơ",
+    artist: "Mer",
+    img: "music-3",
+    src: "music-3"
+  },
+
+];
